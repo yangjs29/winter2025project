e666a4f09efca6084601976e1119b8bfb8fbf433
Shawn Park <shawnpark@10-249-117-205.wireless.oregonstate.edu>
2025-01-16 12:55:57 -0800

    Corner event base

---
+++ Corner Event/corner2.hpp
@@ -0,0 +1,23 @@
+#ifndef CORNER2_CPP
+#define CORNER2_CPP
+#include "corner_event.hpp"
+
+//코너 2 : 도서관(무인도). 
+//도서관에 플레이어가 진입하게 되면, 두 주사위가 같은 수가 나올 때까지
+//도서관에서 3턴 동안 나갈 수 없게 되는데, 이를 관리하는 클래스
+class corner2 : public corner_event{
+    public:
+        //플레이어를 움직임을 3턴 동안 제한하는 함수. 플레이어가 주사위를 던져
+        //두 수가 같은 수가 나왔을 경우 탈출이 가능해야 하므로, 주사위 값 변수를 받아야함.
+        void encounter() override;
+
+        //유저에게 도서관에 도착했으므로, 3턴을 쉬어야 한다는 메세지 출력
+        //만약 두 주사위의 수가 같을 경우 탈출할 수 있다는 메세지 출력
+        void percepts() override;
+
+        //도서관이라는 글씨를 맵에 표시.
+        //도서관이라는 단어를 리턴.
+        char print_name() override;
+};
+
+#endif
---
+++ Corner Event/corner_event.hpp
@@ -0,0 +1,27 @@
+#ifndef CORNER_EVENT_CPP
+#define CORNER_EVENT_CPP
+#include "event.hpp"
+
+//코너 이벤트 클래스
+//코너 이벤트 4개를 관리하는 부모 객체이며,
+//유저가 어떤 코너에 도착했는지 판단하고, 리턴하는 클래스.
+class corner_event : public event{
+    public:
+        //유저가 도착한 코너가 몇 번째 코너인지 리턴하는 함수.
+        //해당 함수에서 리턴한 값에 따라 실행되는 코너 이벤트가 정해짐.
+        void encounter() override;
+
+        //유저에게 코너 칸에 도착했다고 알림.
+        void percepts() override;
+
+
+        //해당 코너가 어떤 효과를 가지고 있는지 적용.
+        virtual void encounter() = 0;
+
+        //해당 코너의 이벤트에 대한 설명
+        virtual void percepts() = 0;
+
+        //해당 코너의 이름을 출력.
+        virtual char print_name() = 0;
+};
+#endif
